f478f0e6a760c967d1a43a3c61a18808d0ff1679
Vetales-a <vetales-a@yandex.ru>
2023-01-31 16:11:23 +0700

    Init project

---
+++ app/src/main/java/ru/synergy/androidstartproject/MainActivity.java
@@ -0,0 +1,18 @@
+package ru.synergy.androidstartproject;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.widget.TextView;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        //setContentView(R.layout.activity_main);
+
+
+    }
+
+}
